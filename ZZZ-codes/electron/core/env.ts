aa9508ad3b3c8474c184da9496d98ff59d58f629
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-10-07 22:52:38 -0500

    feat: agregar archivo env.ts con configuración de API y constantes

---
+++ ZZZ-codes/electron/core/env.ts
@@ -0,0 +1,6 @@
+// electron/core/env.ts
+export const API_BASE = "https://api.ennead.cc/mihoyo";
+export const USER_AGENT = "ZZZ-Codes/1.0";
+export const TTL = {
+    CODES: 15 * 60, // 15min
+} as const;
