2e1a09594cdec7842f84a6753fd77b0892259c7f
LavenderEdit <gercermagden@gmail.com>
2025-10-10 11:37:11 -0500

    Moved the types.ts to the common folder

---
+++ electron/common/types.ts
@@ -0,0 +1,13 @@
+// common/types.ts
+export type GameId = "genshin" | "starrail" | "honkai" | "themis" | "zenless";
+
+export type CodeItem = {
+    code: string;
+    rewards: string[];
+    [k: string]: unknown;
+};
+
+export type CodesPayload = {
+    active: CodeItem[];
+    inactive: CodeItem[];
+};
